9eafe7fd1169709e684c3573d7ab77a550881736
cryptomanic <deepakyadav.iitbhu@gmail.com>
2015-12-29 23:52:11 +0530

    add recent tweets facility

--- server.R
+++ server.R
@@ -1,40 +1,44 @@
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 library(tweeteR)
 
 shinyServer(function(input, output) {
-  
+
   key     <- readline("Enter your API Key : ")
   secret  <- readline("Enter your API Secret : ")
   token   <- tweetOauth(key, secret)
-  
+
   token <- tweetOauth(key, secret)
-  
+
   output$tweets <- renderTable({
     input$dispByHt
     isolate({
       data <- hashTag(token, input$hashtag, input$count)
       img_urls<- vector("character")
-      
+
       for(url in img_profile) {
         img_urls <- append(img_urls, as.character(img(src = url)))
       }
-      
+
       cbind(pic = img_urls, data[,-1])
     })
-    
+
   }, sanitize.text.function = function(x) x)
-  
+
   output$userinfo <- renderTable({
     data <- userInfo(token, input$username)
+    if (is.null(data$id)) {
+      data <- userInfo(token, "narendramodi")
+    }
     pic <- profile_pic
     if (! is.null(pic)) cbind(pic = as.character(img(src = pic)), data.frame(data))
-  }, sanitize.text.function = function(x) x)
-  
+  },  sanitize.text.function = function(x) x)
+
+  output$usertweets <- renderTable({
+    data <- userTweets(token, input$username, count = input$counter)
+    if (length(data) == 0) {
+      data <- userTweets(token, "narendramodi")
+    }
+    data.frame(`Recent Tweets` = data)
+  })
+
 })
